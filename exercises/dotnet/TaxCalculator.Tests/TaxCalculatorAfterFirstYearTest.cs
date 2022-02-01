50572f2d9a11949532dafe5ed3b733e8280a570f
Dalen Chan <Dalen.Chan@brighthr.com>
2022-02-01 10:07:38 +0000

    Disabled FirstYear tests

--- exercises/dotnet/TaxCalculator.Tests/TaxCalculatorAfterFirstYearTest.cs
+++ exercises/dotnet/TaxCalculator.Tests/TaxCalculatorAfterFirstYearTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 
 namespace TaxCalculator.Tests
 {
+    [Ignore("Not yet implemented")]
     public class TaxCalculatorAfterFirstYearTest
     {
         private static readonly DateTime FirstOfApril2017 = new DateTime(2017, 4, 1);
